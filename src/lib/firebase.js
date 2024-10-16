004529aeabcd268b8b3341ff736071636d6bc652
Parag soni <parag.soni@forwhen.com>
2024-10-16 11:44:54 +0530

    first commit

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQW7Ue3m4Amx9VX-I2fEKrChN_wUNpyoE",
+  authDomain: "discord-java-fcd1e.firebaseapp.com",
+  projectId: "discord-java-fcd1e",
+  storageBucket: "discord-java-fcd1e.appspot.com",
+  messagingSenderId: "446967954544",
+  appId: "1:446967954544:web:883557cdf3fe77aeb348b5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
+export const storage = getStorage()
